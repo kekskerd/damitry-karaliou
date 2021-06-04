b5187baa721485c3d01c86be36e6c9463dc7b4d6
kekskerd <kekskerd@gmail.com>
2021-06-04 16:49:11 +0300

    task 7 done

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -35,5 +35,14 @@ public class App {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
+
+        System.out.println("----------task 7----------");
+        int task7Arr[] = new int[100];
+        for (int i = 0; i < task7Arr.length; i++){
+            task7Arr[i] = i + 1;
+        }
+        for (int i = 0; i < task7Arr.length; i++) {
+            System.out.print(task7Arr[i] + ", ");
+        }
     }
 }
