671a4c1c2c1a2cfff74e57dfaed9227d3c0e1c92
kekskerd <kekskerd@gmail.com>
2021-06-07 12:32:08 +0300

    refactoring exeption message

--- src/main/java/org/example/MyArrayDataException.java
+++ src/main/java/org/example/MyArrayDataException.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class MyArrayDataException extends NumberFormatException {
+    public MyArrayDataException() {
+    };
 
     public MyArrayDataException(String message) {
         super(message);
