f1bbbd3ca045a5959bfcd8cecc42f0a5dc561dae
kekskerd <kekskerd@gmail.com>
2021-06-09 10:39:30 +0300

    Task 1 done

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,31 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        ArrayList<String> words = new ArrayList<>(Arrays.asList(
+                "aaa", "ccc", "qqq", "qqq", "eee",
+                "aaa", "fff", "ccc", "ggg", "hhh",
+                "aaa", "aaa", "ccc", "ggg", "hhh",
+                "aaa", "fff", "ccc", "ggg", "aaa",
+                "aaa", "fff", "qqq", "ggg", "hhh"));
+        System.out.println("----TASK 1-----");
+        HashMap<String, Integer> map = countWords(words);
+        for (HashMap.Entry<String, Integer> i : map.entrySet()){
+            System.out.println("Колличество слов " + i.getKey() + " = " + i.getValue());
+        }
+        System.out.println("----TASK 2-----");
+
+    }
+
+    public static HashMap<String, Integer> countWords(ArrayList<String> words) {
+        HashMap<String, Integer> wordsAndCounts = new HashMap<>();
+        for (String word : words) {
+            wordsAndCounts.put(word, Collections.frequency(words, word));
+        }
+        return wordsAndCounts;
     }
 }
