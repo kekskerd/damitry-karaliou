4bcc01197b0aaca28e97cc7a94873b3e970fc388
Korolev Dmitriy <kekskerd@gmail.com>
2021-06-10 22:44:47 +0300

    Task 2 done

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -18,33 +18,44 @@ public class App {
         Course course6 = new Course("Немецкий");
         Course course7 = new Course("Японский");
 
-    List<Student> students = new ArrayList<>(Arrays.asList(
-            new Student("Василий", Arrays.asList(course1, course2, course3, course4, course5)),
-            new Student("Иван", Arrays.asList(course1, course2, course3, course4, course7)),
-            new Student("Олег", Arrays.asList(course2, course3, course4,course6)),
-            new Student("Игорь", Arrays.asList(course1, course2, course3, course4, course5, course6, course7)),
-            new Student("Саша", Arrays.asList(course1,course5, course6, course7)),
-            new Student("Кирилл", Arrays.asList( course2, course3, course4))));
+        List<Student> students = new ArrayList<>(Arrays.asList(
+                new Student("Василий", Arrays.asList(course1, course2, course3, course4, course5)),
+                new Student("Иван", Arrays.asList(course1, course2, course3, course4, course7)),
+                new Student("Олег", Arrays.asList(course2, course3, course4, course6)),
+                new Student("Игорь", Arrays.asList(course1, course2, course3, course4, course5, course6, course7)),
+                new Student("Саша", Arrays.asList(course1, course5, course6, course7)),
+                new Student("Кирилл", Arrays.asList(course2, course3, course4))));
         System.out.println("Task 1");
         List<Course> unique = uniqueCourses(students);
         System.out.println(unique);
+        System.out.println("Task 2");
+        List<Student> threeStudents = bestStudents(students);
+        System.out.println(threeStudents);
         System.out.println("Task 3");
         List<Student> listOfStudents = studentsInCourse(students, "Японский");
         System.out.println(listOfStudents);
 
     }
-        public static List<Student> studentsInCourse(List<Student> students, String course){
+
+    public static List<Course> uniqueCourses(List<Student> students) {
+        return students.stream()
+                .map(Student::getCourses)
+                .flatMap(List::stream)
+                .distinct().collect(Collectors.toList());
+    }
+
+    public static List<Student> bestStudents(List<Student> students) {
+        return students.stream()
+                .sorted((o1, o2) -> o1.getCourses().size() - o2.getCourses().size())
+                .skip(students.size() - 3)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Student> studentsInCourse(List<Student> students, String course) {
         return students.stream()
                 .filter(s -> s.getCourses().toString().contains(course))
                 .collect(Collectors.toList());
     }
 
 
-
-    public static List<Course> uniqueCourses(List<Student> students){
-       return students.stream()
-                .map(Student::getCourses)
-                .flatMap(List::stream)
-                .distinct().collect(Collectors.toList());
-    }
 }
